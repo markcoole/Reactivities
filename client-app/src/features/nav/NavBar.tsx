931414c9a38e1c2c4b0d03d072a184f954eada15
Mark Coole <mark.coole@crafted.co.uk>
2021-02-07 22:54:42 +0000

    develop - implement CRUD operations in React

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+interface IProps {
+    openCreateForm: () => void;
+}
+
+export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+    return (
+        <Menu fixed="top" inverted>
+            <Container>
+                <Menu.Item header>
+                    <img
+                        src="/assets/logo.png"
+                        alt="logo"
+                        style={{ marginRight: "10px" }}
+                    />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item>Activities</Menu.Item>
+                <Menu.Item>
+                    <Button positive onClick={openCreateForm}>
+                        Create activity
+                    </Button>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    );
+};
